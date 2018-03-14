b4b8df8dd8e187278bdb5b1b84d7e50b116ef5b5
LUIZ ROBERTO DE SOUZA OLIVEIRA <luiz.rsoliveira2@senacsp.edu.br>
2018-03-14 11:53:35 -0300

    Initial commit

---
+++ app/src/main/java/com/example/luizrsoliveira2/travel_app/Sign_Up_Activity.java
@@ -0,0 +1,13 @@
+package com.example.luizrsoliveira2.travel_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sign_Up_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_activity);
+    }
+}
---
+++ app/src/main/java/com/example/luizrsoliveira2/travel_app/Splash_Activity.java
@@ -0,0 +1,13 @@
+package com.example.luizrsoliveira2.travel_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+    }
+}
